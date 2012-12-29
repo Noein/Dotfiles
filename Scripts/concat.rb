2d144939f68f99c675723d5d2acf3f5c05a219c5
Noein <noein93@gmail.com>
2012-12-29 17:50:19 +0400

    Rewrite concat.rb

--- Scripts/concat.rb
+++ Scripts/concat.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # coding: UTF-8
 
 # Dotfiles - My configs, backups and scripts.
@@ -5,14 +6,33 @@
 # To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
 # You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-path = "lambda.txt"
-result = temp =  " "
+class StringConcatenator
 
-File.open(path) do |file|
-   while not file.eof?
-   temp = file.gets.to_s
-   result << temp.chomp << " "
-   end
+  def initialize(delimiter, filename = nil)
+    @delimiter = delimiter
+    if filename
+      @input = File.open(filename, 'r')
+    else
+      @input = STDIN
+    end
+  end
+
+  def get_strings
+    strings = []
+    while not @input.eof?
+      strings << @input.gets.to_s.chomp
+    end
+    strings
+  end
+
+  def concat
+    get_strings.join(@delimiter)
+  end
+
 end
 
-puts result
+if ARGV[1]
+  puts StringConcatenator.new(ARGV[0], ARGV[1]).concat
+else
+  puts StringConcatenator.new(ARGV[0] || " ").concat
+end
