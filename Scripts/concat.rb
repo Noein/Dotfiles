af1ab6834c1e865bcd9bfb772d0d397cabcd833a
Noein <noein93@gmail.com>
2012-12-30 18:37:45 +0400

    LOL "Keep it simple, stupid!" Scripts/concat.rb

--- Scripts/concat.rb
+++ Scripts/concat.rb
@@ -6,47 +6,10 @@
 # To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
 # You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-module Concatenable
-
-  def concat
-    get_strings.join(@delimiter)
-  end
-
-private
-  def get_strings
-    strings = []
-    while not @input.eof?
-      strings << @input.gets.to_s.chomp
-    end
-    strings
-  end
-
-end
-
-class StringFileConcatenator
-
-  include Concatenable
-
-  def initialize(delimiter, filename)
-    @delimiter = delimiter
-    @input = File.open(filename, 'r')
-  end
-
-end
-
-class StringStdinConcatenator
-
-  include Concatenable
-
-  def initialize(delimiter)
-    @delimiter = delimiter
-    @input = STDIN
-  end
-
-end
-
 if ARGV[1]
-  puts StringFileConcatenator.new(ARGV[0], ARGV[1]).concat
+  strings = File.open(ARGV[1], 'r').readlines
 else
-  puts StringStdinConcatenator.new(ARGV[0] || " ").concat
+  strings = STDIN.readlines
 end
+
+puts strings.map {|x| x.chomp }.join(ARGV[0])
